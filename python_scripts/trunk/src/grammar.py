5303bc40f3c67e5733ae408f6ed302348b551dd4
riannucci <riannucci@a132214a-332c-0410-9802-916dab326f59>
2007-03-28 23:33:59 +0000

    changed rules a bit.
    git-svn-id: svn://svn.code.sf.net/p/simple-xml/code@8 a132214a-332c-0410-9802-916dab326f59

--- python_scripts/trunk/src/grammar.py
+++ python_scripts/trunk/src/grammar.py
@@ -2,28 +2,27 @@ from simpleparse.common import numbers, strings, comments
 from simpleparse.parser import Parser
 
 declaration = r'''
-smx                 := smx_pi?, EOL*, pi_tag*, EOL*, ((star_tag/name_tag/cust_tag), (smx_node/EOL)*)?, EOL* ,EOF!
-smx_pi              := c'smx',!, SO, str, SO, '?>'!, comment?, EOL!
+smx                 := smx_pi?, EOL*, pi_tag*, EOL*, ((star_tag/name_tag), (smx_node/EOL)*)?, EOL* ,EOF!
+smx_pi              := c'smx',!, SO, str, SO, '?>'!, EOL!
 
-name_tag            := level, name_tag1, ('&&', SO, name_tag1)*, '>', !, SO, (str/text)?, comment?, EOL!
-name_tag1           := ns, name, (SR, attributes)?, SO
+name_tag            := name_tagm,  '>', !, SO, (str/text)?, comment?, EOL!
+name_tagm           := ns, name, (SR, attributes)?, SO, ('&&', SO, name_tagm)*
 
-smx_node            := level, (pi_tag/c_tag/star_tag/el_tag/cust_tag/(comment,EOL!)/((str/text),EOL!))
+smx_node            := level, !, (pi_tag/c_tag/star_tag/el_tag/cust_tag/(comment,EOL!)/((str/text),EOL!))
 
 pi_tag              := name, SO, str, SO, '?>', comment?, EOL!
 
 c_tag               := str, SO, 'C>', comment?, EOL!
 
-star_tag            := star_tag1, ('&&', SO, star_tag1)*, ('*>'/'**>'), comment?, EOL!
-star_tag1           := (star_attributes/((ns,(wild/name)),(SR,star_attributes)?))?, SO
+star_tag            := star_tagm, ('*>'/'**>'), comment?, EOL!
+star_tagm           := (star_attributes/((ns,(wild/name)),(SR,star_attributes)?))?, SO, ('&&', SO, star_tagm)?
 
-el_tag              := el_tag1, ('&&', SO, el_tag1)*, '>', !, SO, (str/text)?, comment?, EOL!
-el_tag1             := (attributes/((ns,name),(SR,attributes)?))?, SO
+el_tag              := el_tagm, '>', !, SO, (str/text)?, comment?, EOL!
+el_tagm             := (attributes/((ns,name),(SR,attributes)?))?, SO, ('&&', SO, el_tagm)?
 
 cust_tag            := (ns, name)? , SO, str, SO, tag_end, comment?, EOL!
 
 comment             := SO,'#',-'\n'*
-#comment_line        := SO,'#',-'\n'*,'\n'
 
 tag_end             := (([\041-\075\077-\176])*,'>')+
 
@@ -44,7 +43,7 @@ name                := (let/'_'),(name_char)*
 <let>               := letter
 <dig>               := digit
 <name_char>         := let/dig/'.'/'-'/'_'
-level               := S*
+level               := SR
 <SO>                := S*
 <SR>                := S+
 <S>                 := [ \t]
