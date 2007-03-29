481a010d088bb151237e8d3e06fbaf2f790bb8ea
riannucci <riannucci@a132214a-332c-0410-9802-916dab326f59>
2007-03-29 06:11:20 +0000

    major parsing edits.. almost working
    git-svn-id: svn://svn.code.sf.net/p/simple-xml/code@10 a132214a-332c-0410-9802-916dab326f59

--- python_scripts/trunk/src/grammar.py
+++ python_scripts/trunk/src/grammar.py
@@ -2,23 +2,35 @@ from simpleparse.common import numbers, strings, comments
 from simpleparse.parser import Parser
 
 declaration = r'''
-smx                 := smx_pi?, EOL*, pi_tag*, EOL*, ((star_tag/name_tag), (smx_node/EOL)*)?, EOL* ,EOF!
+smx                 := preamble, EOL*, body?, EOL* ,EOF!
 smx_pi              := c'smx',!, SO, str, SO, '?>'!, EOL!
 
-name_tag            := name_tagm,  '>', !, SO, (str/text)?, comment?, EOL!
-name_tagm           := ns, name, (SR, attributes)?, SO, ('&&', SO, name_tagm)*
+preamble            := smx_pi?, EOL*, pi_tag*
 
-smx_node            := level, !, (pi_tag/c_tag/star_tag/el_tag/cust_tag/(comment,EOL!)/((str/text),EOL!))
+body                := ((star_tag/name_tag), (EOL/smx_node)*)
+
+>name_tag<          := name_tagm, '>', !, SO, el_tag_tail,  EOL!
+name_tagm           := name_tag1, name_extra?
+name_tag1           := ns_name, (SR, attributes)?, SO
+>name_extra<        := ('&&', SO, name_tag1)*
+
+>smx_node<          := level, !, (pi_tag/c_tag/star_tag/el_tag/cust_tag/(comment,EOL!)/((str/text),EOL!))
 
 pi_tag              := name, SO, str, SO, '?>', comment?, EOL!
 
 c_tag               := str, SO, 'C>', comment?, EOL!
 
-star_tag            := star_tagm, ('*>'/'**>'), comment?, EOL!
-star_tagm           := (star_attributes/((ns,(wild/name)),(SR,star_attributes)?))?, SO, ('&&', SO, star_tagm)?
+>star_tag<          := star_tagm, ('*>'/'**>'), star_tag_tail, EOL!
+star_tagm           := star_tag1, star_extra?
+star_tag1           := (star_attributes/(ns_wild_name),(SR,star_attributes)?)?, SO 
+star_tag_tail       := comment?
+>star_extra<        := ('&&', SO, star_tag1)*
 
-el_tag              := el_tagm, '>', !, SO, (str/text)?, comment?, EOL!
-el_tagm             := (attributes/((ns,name),(SR,attributes)?))?, SO, ('&&', SO, el_tagm)?
+>el_tag<            := el_tagm, '>', !, SO, el_tag_tail, EOL!
+el_tagm             := el_tag1, el_extra?
+>el_extra<          := ('&&', SO, el_tag1)*
+el_tag1             := (attributes/((ns_name),(SR,attributes)?))?, SO
+el_tag_tail         := (str/text)?, comment?
 
 cust_tag            := (ns, name)? , SO, str, SO, tag_end, comment?, EOL!
 
@@ -28,16 +40,19 @@ tag_end             := (([\041-\075\077-\176])*,'>')+
 
 wild                := '*'
 counter             := '#'
+
+ns_name             := ns, name
+ns_wild_name        := ns, (wild/name)
+
 ns                  := (((let/'_'),(let/dig/'_')*)?,':')*
-attribute           := ns, name, SO, EQ, SO, str
+attribute           := ns_name, SO, EQ, SO, str
 >attributes<        := attribute, (SO,attribute)*
 >star_attributes<   := star_attribute, (SO, star_attribute)*
 star_attribute      := ns, name, SO, EQ, SO, (counter/str)
 text                := (let/dig/punctchar/S)+
-str                 := str1
-<str1>              := string
+>str<               := string
 <punctchar>         := punctuationchar
-<EOL>               := SO, '\n'
+EOL                 := SO, '\n'
 <EQ>                := '='
 name                := (let/'_'),(name_char)*
 <let>               := letter
@@ -48,5 +63,6 @@ level               := SR
 <SR>                := S+
 <S>                 := [ \t]
 '''
+    
 
 parser = Parser(declaration, "smx")
--- python_scripts/trunk/src/parse.py
+++ python_scripts/trunk/src/parse.py
@@ -1,18 +1,114 @@
 #!/usr/bin/python
 
 from grammar import parser
-import pprint, sys
-from sys import argv
 import simpleparse
+from simpleparse.processor import Processor
+from simpleparse.stt.TextTools import TextTools
+from sys import exit
 
-pp = pprint.PrettyPrinter(indent=2, width=120)
+class smxProcessor(Processor):
+    def levelMethod(self, taglist, txt, s, f, sub):
+        dpth = f-s
+        if dpth > self.curdpth():
+            self.level_stack += [(dpth, self.curlvl()+1)]
+            taglist += ["indent"]
+        elif dpth < self.curdpth():
+            tmp = self.level_stack
+            if len(self.level_pile_stk) <> 0:
+                for x in xrange(self.level_pile_stk[-1]):
+                    taglist += ['dedent']
+                del self.level_pile_stk[-1]
+            while len(tmp) > 0 and dpth < self.curdpth(tmp):
+                del tmp[-1]
+                taglist += ["dedent"]
+            if len(tmp) == 0 or dpth <> self.curdpth(tmp):
+                print "\nIndentation error in", argv[1]+":"+str(self.line)
+                print "unindent does not match any outer indentation level"
+                exit()
+            else:
+                self.level_stack = tmp  
+        else:
+            if len(self.level_pile_stk) <> 0:
+                for x in xrange(self.level_pile_stk[-1]):
+                    taglist += ['dedent']
+                del self.level_pile_stk[-1]
 
-ex2 = open(argv[1]).read()
+    def curlvl(self, stk=None):
+        if stk == None: stk = self.level_stack
+        return stk[-1][1]
 
-try:
-    ex2p = parser.parse(ex2, processor=None)
-    pp.pprint(ex2p)
-except simpleparse.error.ParserSyntaxError, x:
-    print x
+    def curdpth(self, stk=None):
+        if stk == None: stk = self.level_stack
+        return stk[-1][0]
+    
+    def EOLMethod(self, taglist, txt, s, f, sub):
+        self.line += 1
+    
+    def stringMethod(self, taglist, txt, s, f, sub):
+        cs = sub[0][3]
+        res = ""
+        for x in cs:
+            res += str(eval('"'+txt[x[1]:x[2]]+'"'))
+        taglist += [('str',res)]
+
+    def textMethod(self, taglist, txt, s, f, sub):
+        taglist += [('text', txt[s:f])]
+
+    def bodyMethod(self, taglist, txt, s, f, sub):
+        taglist += [('body',s,f,sub)]
+        for x in self.level_stack:
+            sub += ['dedent']
 
 
+    def tailMethod(self, taglist, txt, s, f, sub):
+        if s <> f:
+            if sub[0][0] <> 'comment':
+                taglist += ["indent"]
+                taglist += sub
+                taglist += ["dedent"]
+            else:
+                taglist += sub
+                
+    def attributeMethod(self, taglist, txt, s, f, sub):
+        taglist += [('attribute', sub[0], sub[1])]
+
+    def name_extraMethod(self, taglist, txt, s, f, sub):
+        print taglist
+        print sub
+
+    def ns_nameMethod(self, taglist, txt, s, f, sub):
+        taglist += [('ns_name', sub[0], sub[1])]
+
+    level_stack = [(0,0)]
+    level_pile_stk = []
+    line = 0
+
+    _m_body   = bodyMethod
+    _m_string   = stringMethod
+    _m_EOL   = EOLMethod
+    _m_text  = textMethod
+    _m_level = levelMethod
+    _m_ns    = TextTools.AppendMatch
+    _m_name    = TextTools.AppendMatch
+    _m_el_tag_tail = tailMethod
+    _m_star_tag_tail = tailMethod
+    _m_ns_name  = ns_nameMethod
+    _m_attribute = attributeMethod
+    
+
+def parse(str):
+    try:
+        rep = parser.parse(ex2, processor = smxProcessor())
+    except simpleparse.error.ParserSyntaxError, x:
+        print x
+        return None
+
+    return rep
+
+if __name__ == "__main__":
+    from sys import argv
+    from pprint import PrettyPrinter
+    pp = PrettyPrinter(indent=2, width=120).pprint
+    ex2 = open(argv[1]).read()
+    result = parse(ex2)
+    pp(result)
