3a71c2fc3b6d144f67f40e73118b16f37e49a43d
riannucci <riannucci@a132214a-332c-0410-9802-916dab326f59>
2007-03-29 07:54:18 +0000

    still not working, but attempting to get levels working properly
    git-svn-id: svn://svn.code.sf.net/p/simple-xml/code@11 a132214a-332c-0410-9802-916dab326f59

--- python_scripts/trunk/src/parse.py
+++ python_scripts/trunk/src/parse.py
@@ -15,23 +15,21 @@ class smxProcessor(Processor):
         elif dpth < self.curdpth():
             tmp = self.level_stack
             if len(self.level_pile_stk) <> 0:
-                for x in xrange(self.level_pile_stk[-1]):
-                    taglist += ['dedent']
+                taglist += ['dedent']*self.level_pile_stk[-1]
                 del self.level_pile_stk[-1]
             while len(tmp) > 0 and dpth < self.curdpth(tmp):
                 del tmp[-1]
-                taglist += ["dedent"]
+                taglist += ['dedent']
             if len(tmp) == 0 or dpth <> self.curdpth(tmp):
                 print "\nIndentation error in", argv[1]+":"+str(self.line)
                 print "unindent does not match any outer indentation level"
                 exit()
             else:
                 self.level_stack = tmp  
-        else:
-            if len(self.level_pile_stk) <> 0:
-                for x in xrange(self.level_pile_stk[-1]):
-                    taglist += ['dedent']
-                del self.level_pile_stk[-1]
+        elif self.close and len(self.level_pile_stk) <> 0:
+            taglist += ['dedent']*self.level_pile_stk[-1]
+            del self.level_pile_stk[-1]
+        self.close = False
 
     def curlvl(self, stk=None):
         if stk == None: stk = self.level_stack
@@ -45,11 +43,7 @@ class smxProcessor(Processor):
         self.line += 1
     
     def stringMethod(self, taglist, txt, s, f, sub):
-        cs = sub[0][3]
-        res = ""
-        for x in cs:
-            res += str(eval('"'+txt[x[1]:x[2]]+'"'))
-        taglist += [('str',res)]
+        taglist += [('str',eval(txt[s:f]))]
 
     def textMethod(self, taglist, txt, s, f, sub):
         taglist += [('text', txt[s:f])]
@@ -59,26 +53,35 @@ class smxProcessor(Processor):
         for x in self.level_stack:
             sub += ['dedent']
 
-
     def tailMethod(self, taglist, txt, s, f, sub):
         if s <> f:
             if sub[0][0] <> 'comment':
-                taglist += ["indent"]
+                taglist += ['indent']
                 taglist += sub
-                taglist += ["dedent"]
+                taglist += ['dedent']
             else:
                 taglist += sub
+                if self.close and len(self.level_pile_stk) <> 0:
+                    for x in xrange(self.level_pile_stk[-1]):
+                        taglist += ['dedent']
+                    del self.level_pile_stk[-1]
                 
     def attributeMethod(self, taglist, txt, s, f, sub):
         taglist += [('attribute', sub[0], sub[1])]
 
-    def name_extraMethod(self, taglist, txt, s, f, sub):
-        print taglist
-        print sub
+    def name_tagmMethod(self, taglist, txt, s, f, sub):
+        for x in sub[:-1]:
+            taglist += [x, 'indent']
+        taglist += [sub[-1]]
+        if len(sub) > 1:
+            self.level_pile_stk += [len(sub)]
+            self.close = True
+        print self.level_pile_stk
 
     def ns_nameMethod(self, taglist, txt, s, f, sub):
         taglist += [('ns_name', sub[0], sub[1])]
 
+    close = False
     level_stack = [(0,0)]
     level_pile_stk = []
     line = 0
@@ -94,6 +97,9 @@ class smxProcessor(Processor):
     _m_star_tag_tail = tailMethod
     _m_ns_name  = ns_nameMethod
     _m_attribute = attributeMethod
+    _m_name_tagm = name_tagmMethod
+    _m_el_tagm = name_tagmMethod
+    _m_star_tagm = name_tagmMethod
     
 
 def parse(str):
